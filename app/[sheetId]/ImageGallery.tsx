6abd332a5f13abf773d3bb2b68711ce3b2c243cc
Daniele <daniele.bertella@gmail.com>
2023-11-23 22:43:07 +0000

    Remove scaling attributes from html tag, since they are messing with the layout.

--- app/[sheetId]/ImageGallery.tsx
+++ app/[sheetId]/ImageGallery.tsx
@@ -3,58 +3,12 @@ import {
   Dialog,
   DialogContent,
   DialogDescription,
-  DialogHeader,
-  DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog'
 import { RelistItem } from '@/lib/relistData'
 import { filterNonNullValues, getAttributeValue } from '@/lib/utils'
 import { camelCase } from 'lodash'
 import { useState } from 'react'
-
-// export const ImageBlock = ({
-//   attributes,
-//   item,
-// }: {
-//   attributes: AttributeItem[]
-//   item: RelistItem
-// }) => {
-//   const values = attributes
-//     ?.map((attr: AttributeItem) => ({
-//       ...attr,
-//       itemValue: getAttributeValue(item[camelCase(attr.title)]),
-//     }))
-//     .filter(filterNonNullValues)
-
-//   const [imgUrl, setUrl] = useState<string>('')
-
-//   return values?.length > 0
-//     ? values.map(it => (
-//         <Dialog>
-//           <div
-//             key={it.title}
-//             className="gallery-box h-50  inline-flex overflow-x-scroll no-scrollbar scrolling-touch scroll-smooth"
-//           >
-//             {it.itemValue
-//               .split('\n')
-//               .filter(Boolean)
-//               .map((url: string, i: number) => (
-//                 <div key={url + i}>
-//                   <DialogTrigger onClick={() => setUrl(url.trim())}>
-//                     <img src={url.trim()} className="h-48 rounded mr-4" alt="" />
-//                   </DialogTrigger>
-//                 </div>
-//               ))}
-//             <DialogContent>
-//               <DialogDescription>
-//                 <img src={imgUrl} className="h-full rounded mr-4" alt="" />
-//               </DialogDescription>
-//             </DialogContent>
-//           </div>
-//         </Dialog>
-//       ))
-//     : null
-// }
 
 export const ImageBlock = ({
   attributes,
--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -21,7 +21,7 @@ const DialogOverlay = React.forwardRef<
   <DialogPrimitive.Overlay
     ref={ref}
     className={cn(
-      "fixed inset-0 z-50 bg-background/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+      "fixed top-0 left-0 right-0 inset-0 z-50 bg-background/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
       className
     )}
     {...props}
