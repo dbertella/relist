cb92d98dd9a71e2fe709e36d87c66cb5fafc0261
Daniele Bertella <daniele.bertella@gmail.com>
2023-12-06 23:18:08 +0100

    fix loader not going full screen (#89)

--- app/[sheetId]/loading.tsx
+++ app/[sheetId]/loading.tsx
@@ -5,13 +5,13 @@ export default function Loading() {
     <>
       <Skeleton className="h-2 w-8/12 my-2" />
       <Skeleton className="h-2 w-10/12 mb-4" />
-      <Skeleton className="h-32 w-screen mb-4" />
+      <Skeleton className="h-32 w-full mb-4" />
       <Skeleton className="h-2 w-8/12 mb-2" />
       <Skeleton className="h-2 w-10/12 mb-2" />
       <Skeleton className="h-2 w-9/12 mb-2" />
       <Skeleton className="h-2 w-8/12 mb-2" />
       <Skeleton className="h-2 w-10/12 mb-4" />
-      <Skeleton className="h-16 w-screen mb-4" />
+      <Skeleton className="h-16 w-full mb-4" />
       <Skeleton className="h-2 w-8/12 mb-2" />
       <Skeleton className="h-2 w-10/12 mb-2" />
       <Skeleton className="h-2 w-9/12 mb-2" />
